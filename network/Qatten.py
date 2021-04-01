ad7cce83843e9c6b67c3d6f230b0f105330aea22
WangQHui <1726422946@qq.com>
2021-04-01 11:23:47 +0800

    Update qatten

--- network/Qatten.py
+++ network/Qatten.py
@@ -1,15 +1,14 @@
 import torch.nn as nn
 import torch
 import torch.nn.functional as F
-import  numpy as np
+import numpy as np
+
 
 class Qatten(nn.Module):
     def __init__(self, conf):
         super(Qatten, self).__init__()
 
         self.conf = conf
-        #self.n_agents = conf.n_agents
-        #self.n_actions = conf.n_actions
 
         self.key_extractors = nn.ModuleList()
         self.selector_extractors = nn.ModuleList()
@@ -19,7 +18,7 @@ class Qatten(nn.Module):
             for i in range(self.n_head):
                 selector_nn = nn.Sequential(nn.Linear(conf.state_shape, conf.hyper_hidden_dim),
                                             nn.ReLU(),
-                                            nn.Linear(conf.hyper_hidden_dim, conf.embed_dim))
+                                            nn.Linear(conf.hyper_hidden_dim, conf.mixing_embed_dim))
                 # 查询
                 self.selector_extractors.append(selector_nn)
                 # 加入qs
@@ -43,15 +42,15 @@ class Qatten(nn.Module):
 
         # 用V代替最后一层的偏移量
         self.V = nn.Sequential(nn.Linear(self.state_shape, conf.mixing_embed_dim),
-                                   nn.ReLU(),
-                                   nn.Linear(self.mixing_embed_dim, 1))
+                               nn.ReLU(),
+                               nn.Linear(self.mixing_embed_dim, 1))
 
     def forward(self, agent_qs, states, actions):
         states = states.reshape(-1, self.state_shape)
         # 从全局状态获得agent自己的特征
         unit_states = states[:, :self.unit_dim*self.n_agents]
         unit_states = unit_states.reshape(-1, self.n_agents, self.unit_dim)
-        unit_states = unit_states.permute(1, 0, 2)
+        unit_states = unit_states.permute(1, 0, 2)  # unit_states:(agent_num,batch_size,unit_dim)
 
         # agents_qs:(batch_size, 1, agent_num)
         agent_qs = agent_qs.view(-1, 1, self.n_agents)
@@ -63,18 +62,52 @@ class Qatten(nn.Module):
             # all_head_selectors:(head_num, batch_size, mixing_embed_dim)
             # unit_states:(agent_num,batch_size,unit_dim)
             all_head_keys = [[k_ext(enc) for enc in unit_states] for k_ext in self.key_extractors]
-            # all_head_keys:(head_num, agent_num, embed_dim)
+            # all_head_keys:(head_num, agent_num, mixing_embed_dim)
 
             # calculate attention per head
             head_attend_logits = []
             head_attend_weights = []
             for curr_head_keys, curr_head_selector in zip(all_head_keys, all_head_selectors):
-                # curr_head_keys:(agent_num, batch_size, embed_dim)
-                # curr_head_electors:(batch_size, embed_dim)
+                # curr_head_keys:(agent_num, batch_size, mixing_embed_dim)
+                # curr_head_electors:(batch_size, mixing_embed_dim)
                 # (batch_size, 1, embed_dim) * (batch_size, embed_dim, agent_num)
                 attend_logits = torch.matmul(curr_head_selector.view(-1, 1, self.mixing_embed_dim),
                                              torch.stack(curr_head_keys).permute(1, 2, 0))
                 # attend_logits:(batch_size, 1, agent_num)
-                # scale dot-products 
+                # scale dot-products by size of key (from Attention is All You Need)
+                scaled_attend_logits = attend_logits / np.sqrt(self.mixing_embed_dim)
+                if self.conf.mask_dead:
+                    # actions:(episode_batch, episode_len - 1, agent_num, 1)
+                    actions = actions.reshape(-1, 1, self.n_agents)
+                    # actions:(batch_size, 1, agent_num)
+                    scaled_attend_logits[actions == 0] = - 99999999  # action == 0 意味着 unit is dead
+                attend_weights = F.softmax(scaled_attend_logits, dim=2)  # (batch_size, 1, agent_num)
+
+                head_attend_logits.append(attend_logits)
+                head_attend_weights.append(attend_weights)
+
+            head_attend = torch.stack(head_attend_weights, dim=1)  # (batch_size, self.n_head, self.n_agents)
+            head_attend = head_attend.view(-1, self.n_head, self.n_agents)
+
+            v = self.V(states).view(-1, 1)  # v:(bs, 1)
+            # head_qs:[head_num, bs, 1]
+            if self.conf.weight_head:
+                w_head = torch.abs(self.hyper_w_head(states))  # w_head:(bs, head_num)
+                w_head = w_head.view(-1, self.n_head, 1).repeat(1, 1, self.n_agents)  # w_head: (bs,head_num,self.n_agents)
+                head_attend *= w_head
+
+            head_attend = torch.sum(head_attend, dim=1)
+
+            if not self.conf.state_bias:
+                v *= 0
+
+            # regularize magnitude of attention logits 规范注意力机制对数的大小
+            attend_mag_regs = self.attend_reg_coef * sum((logit ** 2).mean() for logit in head_attend_logits)
+            head_entropies = [(-((probs + 1e-8).log() * probs).squeeze().sum(1).mean()) for probs in head_attend_weights]
+
+            return head_attend, v, attend_mag_regs, head_entropies
+
+
+
 
 
